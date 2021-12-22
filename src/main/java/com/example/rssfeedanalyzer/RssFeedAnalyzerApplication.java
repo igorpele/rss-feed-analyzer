a640e98e1fe59a73eb6d85e5fc7010d376f8b4dc
pelesic <>
2021-12-22 11:34:22 +0100

    spring boot init commit

---
+++ src/main/java/com/example/rssfeedanalyzer/RssFeedAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rssfeedanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssFeedAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssFeedAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rssfeedanalyzer/RssFeedAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rssfeedanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssFeedAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
